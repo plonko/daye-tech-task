b9b65a616944d70579f646b44dd06b6e719e6ff5
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-01-17 12:06:17 +0000

    Basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, waitForElement } from "@testing-library/react";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import rootReducer from "./redux/reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const TestComponent = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+it("should make an async call", async () => {
+  const { container, getByText } = render(TestComponent);
+  console.log("££££", container);
+
+  await waitForElement(() => getByText("texts loaded"));
+  // await wait(() => expect(fetch).toHaveBeenCalledTimes(2))
+  // expect(fetch).toHaveBeenCalledTimes(2);
 });
