759b9c9164479d7e284b3f49da6209ce01be48d8
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-01-17 15:32:03 +0000

    More tests for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,43 @@
 import React from "react";
-import { render, waitForElement } from "@testing-library/react";
+import { render, waitForElement, wait } from "@testing-library/react";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./App";
 import rootReducer from "./redux/reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-const TestComponent = (
+const connectedApp = (
   <Provider store={store}>
     <App />
   </Provider>
 );
 
-it("should have 6 products", async () => {
-  const { getAllByText } = render(TestComponent);
+test("Loading message is shown before products are loaded", async () => {
+  const { getByText } = render(connectedApp);
+
+  expect(getByText("Products loading...")).toBeInTheDocument();
+});
+
+test("When products are loaded there are 6 product titles", async () => {
+  const { getAllByText } = render(connectedApp);
   const productTitleText = await waitForElement(() =>
     getAllByText("Tampon pack")
   );
 
   expect(productTitleText).toHaveLength(6);
+});
+
+test("The loading message is removed", async () => {
+  const { queryByText } = render(connectedApp);
+
+  await wait(() => {
+    expect(queryByText("Products loading...")).not.toBeInTheDocument();
+  });
 });
