6b37cc44c9ff607c2b5dc0159f79a82e3cccecb6
Sebastian Thomas <sebastianjthomas@gmail.com>
2020-01-18 16:31:23 +0000

    Added currency tests

--- src/utils/getCurrencySymbol.test.js
+++ src/utils/getCurrencySymbol.test.js
@@ -1,5 +1,17 @@
 import { getCurrencySymbol } from "./getCurrencySymbol";
 
-test("should ", () => {
-  expect(true).toEqual(false);
+describe("getCurrencySymbol", () => {
+  test("Should return £ by default", () => {
+    const actual = getCurrencySymbol();
+    const expected = "£";
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("Should return $ when passed 'USD'", () => {
+    const actual = getCurrencySymbol("USD");
+    const expected = "$";
+
+    expect(actual).toEqual(expected);
+  });
 });
